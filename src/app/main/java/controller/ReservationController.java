e3cbc5e2eccf98c4982d514acb0103680e266d7b
MikiKru <michal_kruczkowski@o2.pl>
2020-01-14 18:50:46 +0100

    aplikacja d3 - rezerwacja

--- src/app/main/java/controller/ReservationController.java
+++ src/app/main/java/controller/ReservationController.java
@@ -3,6 +3,9 @@ package controller;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
@@ -10,6 +13,9 @@ import javafx.scene.control.Label;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class ReservationController {
     @FXML
@@ -52,8 +58,15 @@ public class ReservationController {
 
     }
     @FXML
-    void logoutAction(ActionEvent event) {
-
+    void logoutAction(ActionEvent event) throws IOException {
+        Stage primaryStage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("/view/loginPage.fxml"));
+        primaryStage.setTitle("Panel logowanie");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+        // zakmnięcie aktualnie otwartego okna
+        Stage stage = (Stage) tfSearch.getScene().getWindow();
+        stage.close();
     }
     @FXML
     void exitAction(ActionEvent event) {
