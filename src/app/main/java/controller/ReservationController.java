02162041f30e2123a887b0726f3f34331cc776d1
MikiKru <michal_kruczkowski@o2.pl>
2020-01-14 18:30:46 +0100

    aplikacja d3 - rezerwacja

---
+++ src/app/main/java/controller/ReservationController.java
@@ -0,0 +1,59 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class ReservationController {
+    @FXML
+    private TextField tfSearch;
+    @FXML
+    private ComboBox<?> cEvent;
+    @FXML
+    private Spinner<?> sNumber;
+    @FXML
+    private CheckBox cbFv;
+    @FXML
+    private TextArea taDescription;
+    @FXML
+    private ComboBox<?> cType;
+    @FXML
+    private Label lblAviable;
+    @FXML
+    private TextArea taFV;
+    @FXML
+    private Button btnSubmit;
+    @FXML
+    private Button btnConfirm;
+
+    @FXML
+    void confirmAction(ActionEvent event) {
+
+    }
+    @FXML
+    void exitAction(ActionEvent event) {
+
+    }
+    @FXML
+    void fvAction(ActionEvent event) {
+
+    }
+    @FXML
+    void logoutAction(ActionEvent event) {
+
+    }
+    @FXML
+    void selectEventAction(ActionEvent event) {
+
+    }
+    @FXML
+    void submitAction(ActionEvent event) {
+
+    }
+}
